1fd635a471f5f4019ba56317406c8d0bf068ca6d
Carlos <carlosmorales1908@gmail.com>
2023-08-03 11:49:57 -0300

    creado el proyecto

---
+++ src/main/java/com/mindhub/homebanking/Client.java
@@ -0,0 +1,13 @@
+package com.mindhub.homebanking;
+
+public class Client {
+    private String firstName;
+    private String lastName;
+
+    public Client() { }
+
+    public Client(String first, String last) {
+        firstName = first;
+        lastName = last;
+    }
+}
