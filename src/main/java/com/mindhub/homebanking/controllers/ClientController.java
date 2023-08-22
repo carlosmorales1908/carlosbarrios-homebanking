0f953812d1e6c300bfc2f83e315173411b890551
Carlos <carlosmorales1908@gmail.com>
2023-08-22 14:57:30 -0300

    se agrego metodo para obtener el cliente logueado

--- src/main/java/com/mindhub/homebanking/controllers/ClientController.java
+++ src/main/java/com/mindhub/homebanking/controllers/ClientController.java
@@ -7,6 +7,7 @@ import com.mindhub.homebanking.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -50,5 +51,12 @@ public class ClientController {
         }
         clientRepository.save(new Client(firstName, lastName, email, passwordEncoder.encode(password)));
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+
+    @GetMapping("/clients/current")
+    public ClientDTO getCurrentClient(Authentication authentication){
+        Client client = clientRepository.findByEmail(authentication.getName());
+        return new ClientDTO(client);
     }
 }
